4992c72abf95d36efc4e6bca440b8cb56bacb5f1
gersonfribeiro <gersonfernandesribeiro48@gmail.com>
2025-08-20 22:14:22 -0300

    Atividade 2, novo projeto

---
+++ toDoList/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { ToDoList } from './components/to-do-list/to-do-list';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
+};
---
+++ toDoList/src/app/components/to-do-list/to-do-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ToDoList',
+  imports: [],
+  templateUrl: './to-do-list.html',
+  styleUrl: './to-do-list.css'
+})
+export class ToDoList {
+
+}
